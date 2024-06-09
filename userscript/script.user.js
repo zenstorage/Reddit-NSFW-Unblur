140c2a8ae14c23499e8d5f591932a7640775ef55
zenstorage <bielhenriquek.k@gmail.com>
2024-06-09 00:51:10 -0300

    Update version

--- userscript/script.user.js
+++ userscript/script.user.js
@@ -9,7 +9,7 @@
 // @grant           GM_getValue
 // @run-at          document-body
 // @noframes
-// @version         2.3.7a
+// @version         2.3.8
 // @author          hdyzen
 // @description     Unblur nsfw in Shreddit
 // @license         MIT
